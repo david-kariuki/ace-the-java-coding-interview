a94902ba10b19f0e1370670fbdab3f9ab728df45
dk <workplace.davidkariuki@gmail.com>
2022-08-12 17:37:45 +0300

    Big O Notation

--- app/src/main/java/AceTheJavaCodingInterview/big_o_notation/BigONestedLoopWithMultiplication.java
+++ app/src/main/java/AceTheJavaCodingInterview/big_o_notation/BigONestedLoopWithMultiplication.java
@@ -73,4 +73,23 @@ public class BigONestedLoopWithMultiplication {
 
 		// Answer -> nlog2(n)
 	}
+
+	private static void withMultiplicationIntermediate() {
+
+			int n = 10;
+			int sum = 0;
+			int j = 1;
+			double pie = 3.14;
+
+			for (int var = 1; var < n; var += 3) {   // O(n/3)
+				System.out.println("Pie: " + pie);    // O(n/3)
+				j = 1;  // O(n/3)
+				while (j < n) { // O((n/3) * (log3 n))
+					sum += 1;      // O((n/3) * (log3 n))
+					j *= 3;        // O((n/3) * (log3 n))
+				}
+			}
+			System.out.println("Sum: " + sum); //O(1)
+
+	}
 }
