ee53e8283e0aa07904e0befd4d399b45ffd65151
David Kariuki <dkaris.k@gmail.com>
2022-08-14 17:46:56 +0300

    Simple class template

---
+++ app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/ClassTemplate.java
@@ -0,0 +1,20 @@
+/**
+ * ClassTemplate Class
+ *
+ * <p>
+ *
+ * @note
+ * @author David Kariuki
+ * @since /8/2022
+ */
+package AceTheJavaCodingInterview.module2_data_structures;
+
+public class ClassTemplate {
+
+  /**
+   * Main method
+   *
+   * @param args - String[]
+   */
+  public static void main(String[] args) {}
+}
