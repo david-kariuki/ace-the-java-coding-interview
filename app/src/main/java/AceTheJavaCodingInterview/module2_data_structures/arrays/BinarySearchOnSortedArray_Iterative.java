8c16ed3a3f8ea6a4c961679e6d2e555d777bd4c0
dk <workplace.davidkariuki@gmail.com>
2022-08-17 14:31:17 +0300

    Implement Binary Search on a Sorted Array

---
+++ app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/arrays/BinarySearchOnSortedArray_Iterative.java
@@ -0,0 +1,66 @@
+/**
+ * BinarySearchOnSortedArray_Iterative Class
+ *
+ * <p>
+ *
+ * @question We are given an array of integers, nums, sorted in ascending order, and an integer
+ *     value, target. If the target exists in the array, return its index. If the target does not
+ *     exist, then return -1.
+ * @author David Kariuki
+ * @since /8/2022
+ */
+package AceTheJavaCodingInterview.module2_data_structures.arrays;
+
+public class BinarySearchOnSortedArray_Iterative {
+
+  /**
+   * Main method
+   *
+   * @param args - String[]
+   */
+  public static void main(String[] args) {
+
+    int[] nums = new int[] {1, 3, 9, 10, 12};
+    int target = 9;
+  }
+
+  /**
+   * Method to perform binary search
+   *
+   * <p>
+   *
+   * @note Time complexity - The time complexity of this solution is logarithmic, O(log n)
+   * @note Space complexity - The space complexity of this solution is constant, O(1).
+   * @param nums - int[]
+   * @param target - int
+   * @return int
+   */
+  static int binarySearch(int[] nums, int target) {
+
+    // Set start
+    int start = 0;
+    int end = nums.length - 1;
+
+    for (int i = 0; i < nums.length; i++) {
+
+      // Finding the mid using floor division
+      // Using this method to prevent an overflow
+      int mid = start + ((end - start) / 2);
+
+      // Target value is present at the middle of the array
+      if (nums[mid] == target) {
+        return nums[mid];
+
+        // Target value is present in the low subarray
+      } else if (nums[mid] > target) {
+        end = mid - 1;
+
+        // Target value is present in the high subarray
+      } else if (target > nums[mid]) {
+        start = mid + 1;
+      }
+    }
+
+    return -1;
+  }
+}
