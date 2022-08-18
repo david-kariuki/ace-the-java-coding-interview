240256ed864b379628cd38ba6b9674383d371c48
dk <workplace.davidkariuki@gmail.com>
2022-08-18 16:38:06 +0300

    feat: tuple
    create Tuple class
    
    Signed-off-by: dk <workplace.davidkariuki@gmail.com>

---
+++ app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/tupples/Tuple.java
@@ -0,0 +1,27 @@
+/**
+ * Tuple Class
+ *
+ * <p>
+ *
+ * @implNote Creating a tuple class as java does not support returning multiple arguments
+ * @author David Kariuki
+ * @since /8/2022
+ */
+package AceTheJavaCodingInterview.module2_data_structures.tupples;
+
+public class Tuple<X, Y> {
+
+  public X x;
+  public Y y;
+
+  /**
+   * Class constructor
+   *
+   * @param x - X
+   * @param y - Y
+   */
+  public Tuple(X x, Y y) {
+    this.x = x;
+    this.y = y;
+  }
+}
