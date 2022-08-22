03d82827cbf7a4059165add0acbb2b8dc7578910
David Kariuki <dkaris.k@gmail.com>
2022-08-23 01:14:14 +0300

    refactor: refactor
    perform minor refactoring
    
    Signed-off-by: David Kariuki <dkaris.k@gmail.com>

--- app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/tuples/Tuple.java
+++ app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/tuples/Tuple.java
@@ -5,7 +5,7 @@
  *
  * @implNote Creating a tuple class as java does not support returning multiple arguments
  * @author David Kariuki
- * @since /8/2022
+ * @since 18/8/2022
  */
 package AceTheJavaCodingInterview.module2_data_structures.tuples;
 
