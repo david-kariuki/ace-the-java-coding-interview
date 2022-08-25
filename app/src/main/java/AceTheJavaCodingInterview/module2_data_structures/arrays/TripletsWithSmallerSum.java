9e926f93d1721155538fa093666c0692c0302afb
dk <workplace.davidkariuki@gmail.com>
2022-08-25 17:05:22 +0300

    feat: triplets sum with smaller sum
    add class with code to search triplets sum with smaller sum
    
    Signed-off-by: dk <workplace.davidkariuki@gmail.com>

---
+++ app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/arrays/TripletsWithSmallerSum.java
@@ -0,0 +1,25 @@
+/**
+ * TripletsWithSmallerSum Class
+ *
+ * <p>
+ *
+ * @question Problem Statement - Given an array arr of unsorted numbers and a target sum, count all
+ *     triplets in it such that arr[i] + arr[j] + arr[k] < target where i, j, and k are three
+ *     different indices. Write a function to return the count of such triplets.
+ * @note Solution -
+ * @note Time Complexity -
+ * @note Space Complexity -
+ * @author David Kariuki
+ * @since 25/8/2022
+ */
+package AceTheJavaCodingInterview.module2_data_structures.arrays;
+
+public class TripletsWithSmallerSum {
+
+  /**
+   * Main method
+   *
+   * @param args - String[]
+   */
+  public static void main(String[] args) {}
+}
