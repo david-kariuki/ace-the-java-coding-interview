a220b32c92c94ab3a5973cc3c64d63e3216321c1
dk <workplace.davidkariuki@gmail.com>
2022-08-25 17:05:43 +0300

    refactor: refactor
    perform minor refactoring
    
    Signed-off-by: dk <workplace.davidkariuki@gmail.com>

--- app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/ClassTemplate.java
+++ app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/ClassTemplate.java
@@ -3,7 +3,19 @@
  *
  * <p>
  *
- * @question
+ * @question Problem Statement - Given an array of unsorted numbers and a target number, find a
+ *     triplet in the array whose sum is as close to the target number as possible, return the sum
+ *     of the triplet. If there are more than one such triplet, return the sum of the triplet with
+ *     the smallest sum.
+ * @example Example 1:
+ *     <p>Input: [-2, 0, 1, 2], target=2 Output: 1 Explanation: The triplet [-2, 1, 2] has the
+ *     closest sum to the target.
+ * @example Example 2:
+ *     <p>Input: [-3, -1, 1, 2], target=1 Output: 0 Explanation: The triplet [-3, 1, 2] has the
+ *     closest sum to the target.
+ * @example Example 3:
+ *     <p>Input: [1, 0, 1, 1], target=100 Output: 3 Explanation: The triplet [1, 1, 1] has the
+ *     closest sum to the target.
  * @note Solution -
  * @note Time Complexity -
  * @note Space Complexity -
