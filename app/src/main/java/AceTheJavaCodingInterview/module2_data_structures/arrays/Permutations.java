0a790cbfb853a705a0799264a8cfd8e089bf69db
dk <workplace.davidkariuki@gmail.com>
2022-08-25 10:08:02 +0300

    feat: permutation
    permutation
    
    Signed-off-by: dk <workplace.davidkariuki@gmail.com>

--- app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/arrays/Permutations.java
+++ app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/arrays/Permutations.java
@@ -28,13 +28,25 @@
  *     <p>Inserting ‘5’ before ‘3’: [5,3,1]
  *     <p>Inserting ‘5’ between ‘3’ and ‘1’: [3,5,1]
  *     <p>Inserting ‘5’ after ‘1’: [3,1,5]
+ *     <p>
+ * @note Time complexity - We know that there are a total of N! permutations of a set with ‘N’
+ *     numbers. In the algorithm above, we are iterating through all of these permutations with the
+ *     help of the two ‘for’ loops. In each iteration, we go through all the current permutations to
+ *     insert a new number in them. To insert a number into a permutation of size ‘N’ will take
+ *     O(N), which makes the overall time complexity of our algorithm O(N*N!).
+ * @note Space complexity - All the additional space used by our algorithm is for the result list
+ *     and the queue to store the intermediate permutations. If you see closely, at any time, we
+ *     don’t have more than N! permutations between the result list and the queue. Therefore, the
+ *     overall space complexity to store N! permutations each containing N elements will be O(N*N!).
  * @author David Kariuki
  * @since 24/8/2022
  */
 package AceTheJavaCodingInterview.module2_data_structures.arrays;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Permutations {
 
@@ -44,6 +56,7 @@ public class Permutations {
    * @param args - String[]
    */
   public static void main(String[] args) {
+
     List<List<Integer>> result = Permutations.findPermutations(new int[] {1, 3, 5});
     System.out.print("Here are all the permutations: " + result);
   }
@@ -57,6 +70,37 @@ public class Permutations {
   private static List<List<Integer>> findPermutations(int[] nums) {
 
     List<List<Integer>> result = new ArrayList<>();
+
+    Queue<List<Integer>> permutations = new LinkedList<>();
+    permutations.add(new ArrayList<>());
+
+    // Traverse array
+    for (int currentNumber : nums) {
+
+      // Take all existing permutations and add the current number to create
+      // new permutations
+      int n = permutations.size();
+
+      for (int i = 0; i < n; i++) {
+
+        // Get head of queue without removing it
+        List<Integer> oldPermutation = permutations.poll();
+
+        // Create new permutation by adding current number at every position
+        for (int j = 0; j <= permutations.size(); j++) {
+
+          assert oldPermutation != null;
+          List<Integer> newPermutation = new ArrayList<>(oldPermutation);
+          newPermutation.add(j, currentNumber);
+
+          if (newPermutation.size() == nums.length) {
+            result.add(newPermutation);
+          } else {
+            permutations.add(newPermutation);
+          }
+        }
+      }
+    }
 
     return result;
   }
