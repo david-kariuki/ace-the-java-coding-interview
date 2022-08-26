6601bb5d82dc9b08fb3a1841983e3bfa8688c34e
dk <workplace.davidkariuki@gmail.com>
2022-08-26 10:24:20 +0300

    fix: permutation
    fix permutation method
    
    Signed-off-by: dk <workplace.davidkariuki@gmail.com>

--- app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/arrays/Permutations.java
+++ app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/arrays/Permutations.java
@@ -7,10 +7,11 @@
  *     <p>Permutation is defined as the re-arranging of the elements of the set.
  * @note Example, {1, 2, 3} has the following six permutations:
  *     <p>{1, 2, 3} {1, 3, 2} {2, 1, 3} {2, 3, 1} {3, 1, 2} {3, 2, 1}
- *     <p>If a set has ‘n’ distinct elements it will have n! n! permutations.
- * @note Solution This problem follows the Subsets pattern and, we can follow a similar Breadth
- *     First Search (BFS) approach. However, unlike Subsets, every permutation must contain all the
- *     numbers.
+ *     <p>If a set has n distinct elements it will have n! n! permutations.
+ * @note Solution This problem follows the Subsets pattern ({@link
+ *     AceTheJavaCodingInterview.module2_data_structures.arrays.Subset}) and, we can follow a
+ *     similar Breadth First Search (BFS) approach. However, unlike Subsets, every permutation must
+ *     contain all the numbers.
  *     <p>Let’s take the example-1 mentioned above to generate all the permutations. Following a BFS
  *     approach, we will consider one number at a time:
  *     <p>1. If the given set is empty then we have only an empty permutation set: []
@@ -23,17 +24,17 @@
  *     in the 4th step from the permutations of the 3rd step?
  *     <p>If we look closely, we will realize that when we add a new number (5), we take each
  *     permutation of the previous step and insert the new number in every position to generate the
- *     new permutations. For example, inserting ‘5’ in different positions of [3,1] will give us the
+ *     new permutations. For example, inserting 5 in different positions of [3,1] will give us the
  *     following permutations:
- *     <p>Inserting ‘5’ before ‘3’: [5,3,1]
- *     <p>Inserting ‘5’ between ‘3’ and ‘1’: [3,5,1]
- *     <p>Inserting ‘5’ after ‘1’: [3,1,5]
+ *     <p>Inserting 5 before 3: [5,3,1]
+ *     <p>Inserting 5 between 3 and 1: [3,5,1]
+ *     <p>Inserting 5 after 1: [3,1,5]
  *     <p>
- * @note Time complexity - We know that there are a total of N! permutations of a set with ‘N’
+ * @note Time complexity - We know that there are a total of N! permutations of a set with N
  *     numbers. In the algorithm above, we are iterating through all of these permutations with the
- *     help of the two ‘for’ loops. In each iteration, we go through all the current permutations to
- *     insert a new number in them. To insert a number into a permutation of size ‘N’ will take
- *     O(N), which makes the overall time complexity of our algorithm O(N*N!).
+ *     help of the two for loops. In each iteration, we go through all the current permutations to
+ *     insert a new number in them. To insert a number into a permutation of size N will take O(N),
+ *     which makes the overall time complexity of our algorithm O(N*N!).
  * @note Space complexity - All the additional space used by our algorithm is for the result list
  *     and the queue to store the intermediate permutations. If you see closely, at any time, we
  *     don’t have more than N! permutations between the result list and the queue. Therefore, the
@@ -72,26 +73,21 @@ public class Permutations {
   public static List<List<Integer>> findPermutations(int[] nums) {
 
     List<List<Integer>> result = new ArrayList<>();
-
     Queue<List<Integer>> permutations = new LinkedList<>();
     permutations.add(new ArrayList<>());
 
-    // Traverse array
     for (int currentNumber : nums) {
 
-      // Take all existing permutations and add the current number to create
-      // new permutations
+      // Take all existing permutations and add the current number to create new
+      // permutations
       int n = permutations.size();
 
       for (int i = 0; i < n; i++) {
-
-        // Get head of queue without removing it
         List<Integer> oldPermutation = permutations.poll();
 
-        // Create new permutation by adding current number at every position
-        for (int j = 0; j <= permutations.size(); j++) {
+        // create a new permutation by adding the current number at every position
+        for (int j = 0; j <= oldPermutation.size(); j++) {
 
-          assert oldPermutation != null;
           List<Integer> newPermutation = new ArrayList<>(oldPermutation);
           newPermutation.add(j, currentNumber);
 
@@ -103,7 +99,6 @@ public class Permutations {
         }
       }
     }
-
     return result;
   }
 }
