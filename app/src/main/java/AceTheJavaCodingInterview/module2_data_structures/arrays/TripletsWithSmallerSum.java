e8541e0be71efe889c8ba7da473287c850ab8077
dk <workplace.davidkariuki@gmail.com>
2022-08-26 09:16:45 +0300

    feat: class template
    update class template
    
    Signed-off-by: dk <workplace.davidkariuki@gmail.com>

--- app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/arrays/TripletsWithSmallerSum.java
+++ app/src/main/java/AceTheJavaCodingInterview/module2_data_structures/arrays/TripletsWithSmallerSum.java
@@ -3,14 +3,12 @@
  *
  * <p>
  *
- * @question Problem Statement - Given an array arr of unsorted numbers and a target sum, count all
- *     triplets in it such that arr[i] + arr[j] + arr[k] < target where i, j, and k are three
- *     different indices. Write a function to return the count of such triplets.
+ * @question Problem Statement -
  * @note Solution -
  * @note Time Complexity -
  * @note Space Complexity -
  * @author David Kariuki
- * @since 25/8/2022
+ * @since /8/2022
  */
 package AceTheJavaCodingInterview.module2_data_structures.arrays;
 
